6fe978bbe1366edb9ef15a9804a6cc5abbf74d84
User <User@LAPTOP-QUFJE5SL>
2023-10-16 01:25:46 +0800

    First Commit

---
+++ MakingMoneyATM/src/com/studyopedia/MakingMoneyATM.java
@@ -0,0 +1,12 @@
+package com.studyopedia;
+
+public class MakingMoneyATM {
+    public static void main(String[] args) {
+        // Display a title and welcome message in an attractive layout
+        System.out.println("**********************************************");
+        System.out.println("*        Making Money Bank ATM Service       *");
+        System.out.println("**********************************************");
+        System.out.println("                   Welcome!                  ");
+    }
+   
+}
